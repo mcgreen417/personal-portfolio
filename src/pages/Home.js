772f326e7cd4cb8fd2b7240b53696b45311988a1
Michaela Green <mcgreen417@knights.ucf.edu>
2021-09-24 23:56:12 -0400

    Added bottom wave decoration and WIP text on the homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,18 @@ import React from 'react';
 import './Style.css';
 import Navbar from '../components/Navbar/Navbar';
 import { ReactComponent as WaveTop } from '../imgs/wave-decoration-top-standard.svg';
+import { ReactComponent as WaveBottom } from '../imgs/wave-decoration-bottom-standard.svg';
 
 function home() {
     return (
         <div>
             <Navbar />
             <WaveTop />
+            <div className="homepageFocus">
+                <h1> Welcome to my website!</h1>
+                <h2>It's currently in progress, so don't mind the mess.</h2>
+            </div>
+            <WaveBottom className="bottomDecoration"/>
         </div>
     )
 }
