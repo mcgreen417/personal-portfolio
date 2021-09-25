7bb237818b54402d8fbd298f6a1394a6616f0fc2
Michaela Green <mcgreen417@knights.ucf.edu>
2021-09-24 23:40:56 -0400

    Add drop shadow to navigation and add top wave decoration

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Style.css';
 import Navbar from '../components/Navbar/Navbar';
+import { ReactComponent as WaveTop } from '../imgs/wave-decoration-top-standard.svg';
 
 function home() {
     return (
         <div>
             <Navbar />
+            <WaveTop />
         </div>
     )
 }
