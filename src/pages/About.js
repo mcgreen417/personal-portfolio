4fa741032a548e7b22ed6453246fe04f34eebffd
Michaela Green <mcgreen417@knights.ucf.edu>
2021-09-25 00:02:48 -0400

    Add placeholder text on About and Projects pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './Style.css';
 import Navbar from '../components/Navbar/Navbar';
+import { ReactComponent as WaveTop } from '../imgs/wave-decoration-top-standard.svg';
+import { ReactComponent as WaveBottom } from '../imgs/wave-decoration-bottom-standard.svg';
 
 function about() {
     return (
         <div>
             <Navbar />
-            <h1>Welcome to my about page!</h1>
+            <WaveTop />
+            <div className="homepageFocus">
+                <h1> Welcome to my About page!</h1>
+                <h2>It's currently in progress, so don't mind the mess.</h2>
+            </div>
+            <WaveBottom className="bottomDecoration"/>
         </div>
     )
 }
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './Style.css';
 import Navbar from '../components/Navbar/Navbar';
+import { ReactComponent as WaveTop } from '../imgs/wave-decoration-top-standard.svg';
+import { ReactComponent as WaveBottom } from '../imgs/wave-decoration-bottom-standard.svg';
 
 function projects() {
     return (
         <div>
             <Navbar />
-            <h1>Welcome to my projects page!</h1>
+            <WaveTop />
+            <div className="homepageFocus">
+                <h1> Welcome to my Projects page!</h1>
+                <h2>It's currently in progress, so don't mind the mess.</h2>
+            </div>
+            <WaveBottom className="bottomDecoration"/>
         </div>
     )
 }
